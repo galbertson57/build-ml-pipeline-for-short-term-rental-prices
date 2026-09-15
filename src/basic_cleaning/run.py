66003dca64227f1676069636a95dc854f8c97349
grace <grace.albertson.snyder1@gmail.com>
2026-09-15 01:04:03 +0000

    Fix basic_cleaning to remove areas outside of NYC.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Drop rows outside of NYC
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save dataframe
     df.to_csv("clean_sample.csv", index=False)
 
